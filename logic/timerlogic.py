0b16b1418cc545a30979e1910e61621bb802b752
Loomis27 <cstojevich@gmail.com>
2025-11-19 18:00:16 -0600

    Functions, timer logic
    Started Functions to get values user selects from a dropbox. Also started timer logic

--- logic/timerlogic.py
+++ logic/timerlogic.py
@@ -1,2 +1,44 @@
 #first push From Cole
+import tkinter 
+
+def GetTimerLength():
+    #get's timer length from input, convert from min to seconds turn into integer
+    minuteValue = int() # inside () put value user selects from dropbox
+    secondsValue = minuteValue * 60
+    pass
+
+def GetBreakLength():
+    #function to get value for break length from input
+    #convert from min to sec's, turn into integer
+    minuteValue = int() #inside () put value user selects from dropbox
+    secondsValue = minuteValue * 60
+    pass
+
+def TimerStart():
+    #function to start the timer
+    pass
+
+def TimerStop():
+    #function to stop timer
+    pass
+
+timerValues = [] # 0 - 59 minutes user can select
+for x in range(60):
+    timerValues.append(x)
+
+print(timerValues)
+"""
+timerLength = GetTimerLength() #assign timerlength to a var
+breakLength = GetBreakLength() #assign breaklength to a var
+
+#logic for if timerlength is greater than 0
+if timerLength > 0:
+    #keep timer running
+    pass
+elif timerLength == 0:
+    #if timerlength is 0 stop timer
+    pass
+"""
+    
+
 
