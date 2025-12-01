6229881dc2141a2377b46d6f77fe464aa6144be4
Loomis27 <cstojevich@gmail.com>
2025-12-01 15:55:28 -0600

    Changed some of the functions for timer. Added storage

--- logic/timerlogic.py
+++ logic/timerlogic.py
@@ -1,44 +1,33 @@
 #first push From Cole
-import tkinter 
+import storage.storage as storage
 
-def GetTimerLength():
-    #get's timer length from input, convert from min to seconds turn into integer
-    minuteValue = int() # inside () put value user selects from dropbox
-    secondsValue = minuteValue * 60
-    pass
+storage.LoadData()
 
-def GetBreakLength():
-    #function to get value for break length from input
-    #convert from min to sec's, turn into integer
-    minuteValue = int() #inside () put value user selects from dropbox
-    secondsValue = minuteValue * 60
-    pass
+workTime = storage.data["work_duration"] #amount of time to work
+breakTime = storage.data["break_duration"] #amount of time for break
 
 def TimerStart():
     #function to start the timer
-    pass
+    try:
+        if workTime > 0:
+    #keep timer running
+            #print work time on screen in timer
+            workTime =- 1
+            
+        elif workTime == 0:
+    #if timerlength is 0 stop timer
+            TimerStop()
+            
+    except:
+        pass
+    
 
 def TimerStop():
     #function to stop timer
-    pass
-
-timerValues = [] # 0 - 59 minutes user can select
-for x in range(60):
-    timerValues.append(x)
-
-print(timerValues)
-"""
-timerLength = GetTimerLength() #assign timerlength to a var
-breakLength = GetBreakLength() #assign breaklength to a var
-
-#logic for if timerlength is greater than 0
-if timerLength > 0:
-    #keep timer running
-    pass
-elif timerLength == 0:
-    #if timerlength is 0 stop timer
-    pass
-"""
-    
-
-
+    try:
+        if workTime > 0:
+            storage.data["work_duration"] = workTime
+        else:
+            pass
+    except:
+        pass
